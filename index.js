ec1e82a3b7d5dd4e97b496fe19413fb5c2616606
Miguel <miguelmail2006@gmail.com>
2019-08-28 19:04:41 -0300

    feature/data-api - Added body-parser and basic data api

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var mongodb = require('mongodb');
+var bodyParser = require('body-parser');
 var mongoClient = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
 var server = express();
 var port = process.env.PORT || 3000;
 var cors = require('cors');
 var mongoUrl = 'mongodb://localhost:27017/';
-var dbname = 'proyecto-perfil';
+var dbname = 'profile';
 
 function getCollection(collectionName) {
   return mongoClient
@@ -18,6 +20,8 @@ function getCollection(collectionName) {
     });
 }
 
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(cors());
 
 server.get('/', function (req, res) {
@@ -25,12 +29,51 @@ server.get('/', function (req, res) {
 });
 
 server.get('/api/data', function (req, res) {
-  getCollection('datos')
+  getCollection('data')
     .then(function (dataCollection) {
       return dataCollection.find({}).toArray();
     })
     .then(function (data) {
       return res.status(200).send(data[0] || {});
+    })
+    .catch(function (error) {
+      console.error(error);
+      return res.status(500).send(error);
+    });
+});
+
+server.post('/api/data', function (req, res) {
+  console.log('BODY', req.body);
+  
+  getCollection('data')
+    .then(async function (collection) {
+      return {
+        collection: collection,
+        result: await collection.find({}).toArray()
+      }
+    })
+    .then(function (response) {
+      console.log('In response from result');
+      console.log(response.result.length);
+      
+      if (response.result.length) { // result.length = 0 == false | result.length > 0 == true
+        response.collection.updateOne({
+          _id: new ObjectID(response.result[0]._id)
+        }, { $set: req.body }, function (error, result) {
+          console.log('error', error);
+          console.log('result', result);
+          
+          if (error) return res.status(500).send(error);
+
+          return res.status(200).send('Se actualizaron los datos');
+        });
+      } else {
+        response.collection.insertOne(req.body, function (error, result) {
+          if (error) return res.status(500).send(error);
+
+          return res.status(200).send('Se salvaron los datos');
+        });
+      }
     })
     .catch(function (error) {
       console.error(error);
